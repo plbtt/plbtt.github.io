e9b360bf0c12074472f521712e229c0cdbdd995a
plbtt <tumaryeskermes@gmail.com>
2025-12-02 00:21:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function openTG() {
-  window.location.href = "https://plbtt.github.io/tg";
+  window.location.href = "https://t.me/+_PzFoPA1DmM1NjIy";
 }
 
 function copyLink() {
-  const url = "https://plbtt.github.io/tg";
+  const url = "https://t.me/+_PzFoPA1DmM1NjIy"; // правильная ссылка
   navigator.clipboard.writeText(url).then(() => {
     alert('Ссылка скопирована! Вставьте в браузер и откройте.');
+  }).catch(() => {
+    alert('Не удалось скопировать ссылку. Попробуйте вручную.');
   });
 }
